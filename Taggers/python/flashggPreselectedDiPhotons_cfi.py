1b9767fae2b6dfdd0e07b696225a42373c5cad09
Fabricio Jimenez <fabricioajm@gmail.com>
2016-04-13 17:06:59 +0200

    flashggPreselectedDiPhotons_cfi.py updated with dummy cuts. Try 2.

--- Taggers/python/flashggPreselectedDiPhotons_cfi.py
+++ Taggers/python/flashggPreselectedDiPhotons_cfi.py
@@ -4,57 +4,61 @@ phoEffArea=cms.PSet( var=cms.string("abs(superCluster.eta)"), bins=cms.vdouble(0
 
 rediscoveryHLTvariables = cms.vstring(
     "pfPhoIso03", 
-    "trkSumPtHollowConeDR03",
-    "full5x5_sigmaIetaIeta",
-    "full5x5_r9",
+#    "trkSumPtHollowConeDR03",
+#    "full5x5_sigmaIetaIeta",
+#    "full5x5_r9",
 #    "passElectronVeto"
     )
 
 #cuts to mimic category trigger cuts
 rediscoveryHLTcutsV1 = cms.VPSet(
-    cms.PSet(cut=cms.string("isEB && full5x5_r9>0.85"), ##EB high R9
+#    cms.PSet(cut=cms.string("isEB && full5x5_r9>0.85"), ##EB high R9
+    cms.PSet(cut=cms.string("isEB && full5x5_r9>=0.0"), ##EB high R9
              selection = cms.VPSet(
             cms.PSet(max=cms.string("100000.0"), 
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(min=cms.string("0.5")),
-#            cms.PSet(min=cms.string("0.5"))
+#            cms.PSet(max=cms.string("100000.0")),
+#            cms.PSet(max=cms.string("100000.0")),
+#            cms.PSet(min=cms.string("0.5")),
+##            cms.PSet(min=cms.string("0.5"))
             ),
              ),
     
-    cms.PSet(cut=cms.string("isEE && full5x5_r9>0.90"),  ##EE high R9
+#    cms.PSet(cut=cms.string("isEE && full5x5_r9>0.90"),  ##EE high R9
+    cms.PSet(cut=cms.string("isEE && full5x5_r9>=0.0"),  ##EE high R9
              selection = cms.VPSet(
             cms.PSet(max=cms.string("100000.0"), 
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(max=cms.string("100000.0")),
-            cms.PSet(min=cms.string("0.8")),
-#            cms.PSet(min=cms.string("0.5"))
+#            cms.PSet(max=cms.string("100000.0")),
+#            cms.PSet(max=cms.string("100000.0")),
+#            cms.PSet(min=cms.string("0.8")),
+##            cms.PSet(min=cms.string("0.5"))
             ),
              ),
-    cms.PSet(cut=cms.string("isEB && full5x5_r9<=0.85"),  #EB low R9
+#    cms.PSet(cut=cms.string("isEB && full5x5_r9<=0.85"),  #EB low R9
+    cms.PSet(cut=cms.string("isEB && full5x5_r9>=0.0"),  #EB low R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"), 
+            cms.PSet(max=cms.string("100000.0"), #Before: 4.0
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("6.0")),
-            cms.PSet(max=cms.string("0.015")),
-            cms.PSet(min=cms.string("0.5")),
-#            cms.PSet(min=cms.string("0.5"))
+#            cms.PSet(max=cms.string("6.0")),
+#            cms.PSet(max=cms.string("0.015")),
+#            cms.PSet(min=cms.string("0.5")),
+##            cms.PSet(min=cms.string("0.5"))
             ),       
              ),       
-    cms.PSet(cut=cms.string("isEE && full5x5_r9<=0.90"),  ##EE low R9
+#    cms.PSet(cut=cms.string("isEE && full5x5_r9<=0.90"),  ##EE low R9
+    cms.PSet(cut=cms.string("isEE && full5x5_r9>=0.0"),  ##EE low R9
              selection = cms.VPSet(
-            cms.PSet(max=cms.string("4.0"), 
+            cms.PSet(max=cms.string("100000.0"), # Before: 4.0 
                      rhocorr=phoEffArea,
                      ),
-            cms.PSet(max=cms.string("6.0")),
-            cms.PSet(max=cms.string("0.035")),
-            cms.PSet(min=cms.string("0.8")),
-#            cms.PSet(min=cms.string("0.5"))
+#            cms.PSet(max=cms.string("6.0")),
+#            cms.PSet(max=cms.string("0.035")),
+#            cms.PSet(min=cms.string("0.8")),
+##            cms.PSet(min=cms.string("0.5"))
             ),
              )
     )
@@ -64,8 +68,8 @@ rediscoveryHLTcutsV1 = cms.VPSet(
 flashggPreselectedDiPhotons = cms.EDFilter(
     "GenericDiPhotonCandidateSelector",
     #src = cms.InputTag("flashggUpdatedIdMVADiPhotons"),
-   src = cms.InputTag("flashggDiPhotons"), 
-   rho = cms.InputTag("fixedGridRhoAll"),
+    src = cms.InputTag("flashggDiPhotons"), 
+    rho = cms.InputTag("fixedGridRhoAll"),
 #    cut = cms.string(
 #        "    (leadingPhoton.full5x5_r9>0.8||leadingPhoton.egChargedHadronIso<20||leadingPhoton.egChargedHadronIso/leadingPhoton.pt<0.3)"
 #        " && (subLeadingPhoton.full5x5_r9>0.8||subLeadingPhoton.egChargedHadronIso<20||subLeadingPhoton.egChargedHadronIso/subLeadingPhoton.pt<0.3)"
@@ -81,7 +85,7 @@ flashggPreselectedDiPhotons = cms.EDFilter(
 #        " && (leadingPhoton.pt > mass/3. && subLeadingPhoton.pt > mass/4.)#low mass run I
 #        " && mass > 55 "#low mass
 #        " && (leadingPhoton.passElectronVeto) && (subLeadingPhoton.passElectronVeto)"
-        ),
+#    ),
     variables = rediscoveryHLTvariables,
     categories = rediscoveryHLTcutsV1
     )
